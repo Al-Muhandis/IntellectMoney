f6d959c796fbc3022f4ca1f5c5009b69c8517039
Renat Suleymanov <mail@renat.su>
2025-12-09 21:51:51 +0400

    lpk

---
+++ intellectmoney.pas
@@ -0,0 +1,15 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit intellectmoney;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  intellectmoney_api, intellectmoney_base, intellectmoney_merchant, intellectmoney_webhooks;
+
+implementation
+
+end.
