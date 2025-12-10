bd6235ce2112c79f60cb777ef7ffe18d05acb1df
Renat Suleymanov <mail@renat.su>
2025-12-11 00:29:46 +0400

    receipts

--- src/intellectmoney_merchant.pas
+++ src/intellectmoney_merchant.pas
@@ -13,29 +13,70 @@ uses
 
 type
 
+  TReceiptPosition = record
+    Quantity: Double;
+    Price: Double;
+    Tax: Integer;        // 1-6 (see doc)
+    Text: string;
+    PaymentSubjectType: Integer;  // 1-13 (optional)
+    PaymentMethodType: Integer;   // 1-7 (optional)
+    SupplierINN: string;          // up to 12 characters (optional)
+  end;
+
+  TReceiptPayment = record
+    PaymentType: Integer;  // 1, 2, 14, 15, 16
+    Amount: Double;
+  end;
+
+  TPaymentsArray = array of TReceiptPayment;
+
   { TIntellectMoneyMerchantClient }
 
   TIntellectMoneyMerchantClient = class(TIntellectMoneyBaseClient)
   private
     function GenerateHash(const aOrderId, aServiceName, aAmount, aCurrency: string): String;
+    function BuildMerchantReceipt(const aINN, aGroup, aCustomerContact: string;
+      const aPositions: array of TReceiptPosition;
+      aTaxationSystem: Integer = 0;
+      const aPayments: TPaymentsArray = nil;
+      aSkipAmountCheck: Integer = 0): string;
   public
     constructor Create(const aEshopId, aSecretKey: string); override;
     function CreatePaymentURL(
       const aOrderId: string;
       const aAmount: Double;
       const aCurrency: string;
-      const aEmail: string;
+      const aEmail: string = '';
       const aUserName: string = '';
       const aServiceName: string = '';
       const aSuccessUrl: string = '';
       const aFailUrl: string = '';
-      const aBackURL: string = ''): string;
+      const aBackURL: string = ''
+    ): string;
+
+    function CreatePaymentURLWithReceipt(
+      const aOrderId: string;
+      const aAmount: Double;
+      const aCurrency: string;
+      const aEmail: string;
+      const aINN: string;
+      const aGroup: string;
+      const aPositions: array of TReceiptPosition;
+      aTaxationSystem: Integer = 0;
+      const aUserName: string = '';
+      const aServiceName: string = '';
+      const aSuccessUrl: string = '';
+      const aFailUrl: string = '';
+      const aBackURL: string = '';
+      const aPayments: TPaymentsArray = nil;
+      aSkipAmountCheck: Integer = 0
+    ): string;
   end;
 
 implementation
 
 uses
-  md5, fphttpclient, opensslsockets, jsonscanner
+  md5, fphttpclient, jsonscanner, fpjson
   ;
 
 var
@@ -58,6 +99,79 @@ begin
 end;
 
 { TIntellectMoneyMerchantClient }
+
+function TIntellectMoneyMerchantClient.BuildMerchantReceipt(const aINN, aGroup, aCustomerContact: string;
+  const aPositions: array of TReceiptPosition; aTaxationSystem: Integer; const aPayments: TPaymentsArray;
+  aSkipAmountCheck: Integer): string;
+var
+  Receipt, Content, CheckClose: TJSONObject;
+  Positions, Payments: TJSONArray;
+  Position, Payment: TJSONObject;
+  i: Integer;
+begin
+  Receipt := TJSONObject.Create;
+  try
+    // Добавляем основные параметры
+    if aSkipAmountCheck <> 0 then
+      Receipt.Add('skipAmountCheck', aSkipAmountCheck);
+
+    Receipt.Add('inn', aINN);
+    Receipt.Add('group', aGroup);
+
+    // Создаем content
+    Content := TJSONObject.Create;
+    Content.Add('type', 1);  // Тип документа: 1 = приход
+    Content.Add('customerContact', aCustomerContact);
+
+    // Добавляем позиции
+    Positions := TJSONArray.Create;
+    for i := Low(aPositions) to High(aPositions) do
+    begin
+      Position := TJSONObject.Create;
+      Position.Add('quantity', aPositions[i].Quantity);
+      Position.Add('price', aPositions[i].Price);
+      Position.Add('tax', aPositions[i].Tax);
+      Position.Add('text', aPositions[i].Text);
+
+      if aPositions[i].PaymentSubjectType > 0 then
+        Position.Add('paymentSubjectType', aPositions[i].PaymentSubjectType);
+
+      if aPositions[i].PaymentMethodType > 0 then
+        Position.Add('paymentMethodType', aPositions[i].PaymentMethodType);
+
+      if aPositions[i].SupplierINN <> '' then
+        Position.Add('supplierINN', aPositions[i].SupplierINN);
+
+      Positions.Add(Position);
+    end;
+    Content.Add('positions', Positions);
+
+    Receipt.Add('content', Content);
+
+    // Добавляем checkClose с платежами (если используется API)
+    if Length(aPayments) > 0 then
+    begin
+      CheckClose := TJSONObject.Create;
+
+      Payments := TJSONArray.Create;
+      for i := Low(aPayments) to High(aPayments) do
+      begin
+        Payment := TJSONObject.Create;
+        Payment.Add('type', aPayments[i].PaymentType);
+        Payment.Add('amount', aPayments[i].Amount);
+        Payments.Add(Payment);
+      end;
+      CheckClose.Add('payments', Payments);
+      CheckClose.Add('taxationSystem', aTaxationSystem);
+
+      Receipt.Add('checkClose', CheckClose);
+    end;
+
+    Result := Receipt.AsJSON;
+  finally
+    Receipt.Free;
+  end;
+end;
 
 function TIntellectMoneyMerchantClient.GenerateHash(const aOrderId, aServiceName, aAmount, aCurrency: string): String;
 var
@@ -104,6 +218,50 @@ begin
     aParams.Values['hash'] := aHash;
 
     Result := Format(Url, [Lang]) +'?'+EncodeURLParams(aParams);
+  finally
+    aParams.Free;
+  end;
+end;
+
+function TIntellectMoneyMerchantClient.CreatePaymentURLWithReceipt(const aOrderId: string; const aAmount: Double;
+  const aCurrency: string; const aEmail: string; const aINN: string; const aGroup: string;
+  const aPositions: array of TReceiptPosition; aTaxationSystem: Integer; const aUserName: string;
+  const aServiceName: string; const aSuccessUrl: string; const aFailUrl: string; const aBackURL: string;
+  const aPayments: TPaymentsArray; aSkipAmountCheck: Integer): string;
+var
+  aParams: TStringList;
+  aHash, aAmountStr, aMerchantReceipt: string;
+begin
+  aParams := TStringList.Create;
+  try
+    aAmountStr := FormatFloat('0.00', AAmount, _FrmtStngsAPI);
+
+    // Формируем чек
+    aMerchantReceipt := BuildMerchantReceipt(
+      aINN, aGroup, aEmail, aPositions, aTaxationSystem, aPayments, aSkipAmountCheck
+    );
+
+    aParams.Values['eshopId'] := EshopId;
+    aParams.Values['orderId'] := aOrderId;
+    aParams.Values['serviceName'] := aServiceName;
+    aParams.Values['recipientAmount'] := aAmountStr;
+    aParams.Values['recipientCurrency'] := aCurrency;
+    aParams.Values['userName'] := aUserName;
+    aParams.Values['user_email'] := aEmail;
+    aParams.Values['successUrl'] := aSuccessUrl;
+    aParams.Values['failUrl'] := aFailUrl;
+    aParams.Values['backUrl'] := aBackURL;
+    aParams.Values['merchantReceipt'] := aMerchantReceipt;
+
+    aHash := GenerateHash(
+      aParams.Values['orderId'],
+      aParams.Values['serviceName'],
+      aParams.Values['recipientAmount'],
+      aParams.Values['recipientCurrency']
+    );
+    aParams.Values['hash'] := aHash;
+
+    Result := Format('%s%s/?%s', [Url, Lang, EncodeURLParams(aParams)]);
   finally
     aParams.Free;
   end;
--- tests/intellectmoneymerchanttests.pas
+++ tests/intellectmoneymerchanttests.pas
@@ -5,8 +5,7 @@ unit IntellectMoneyMerchantTests;
 interface
 
 uses
-  fpcunit, testregistry, Classes, SysUtils, intellectmoney_merchant, intellectmoney_base
-  ;
+  fpcunit, testregistry, Classes, SysUtils, intellectmoney_merchant, intellectmoney_base, fpjson, jsonparser;
 
 type
   TTestIMMerchantClient = class(TIntellectMoneyMerchantClient)
@@ -31,6 +30,15 @@ type
     procedure TestURLEncoding;
     procedure TestAmountFormatting;
     procedure TestLanguageInURL;
+    procedure TestCreatePaymentURLWithReceiptBasic;
+    procedure TestCreatePaymentURLWithReceiptMultiplePositions;
+    procedure TestCreatePaymentURLWithReceiptOptionalFields;
+    procedure TestCreatePaymentURLWithReceiptPayments;
+    procedure TestMerchantReceiptJSONStructure;
+    procedure TestMerchantReceiptWithSkipAmountCheck;
+    procedure TestMerchantReceiptWithTaxationSystem;
+    procedure TestMerchantReceiptUTF8Encoding;
+    procedure TestMerchantReceiptEmptyPositions;
   end;
 
 implementation
@@ -118,16 +126,15 @@ procedure TIntellectMoneyMerchantTest.TestCreatePaymentURLMinimalParameters;
 var
   PaymentURL: string;
 begin
-  // Тест с минимальным набором параметров (остальные пустые)
   PaymentURL := FMerchant.CreatePaymentURL(
     'ORDER-MIN',
     10.00,
     'RUB',
     'minimal@test.com',
-    '',  // userName
-    '',  // serviceName
-    '',  // successUrl
-    ''   // failUrl
+    '', // userName
+    '', // serviceName
+    '', // successUrl
+    ''  // failUrl
   );
 
   AssertTrue(PaymentURL <> '', 'Payment URL should not be empty');
@@ -141,7 +148,6 @@ var
   PaymentURL1, PaymentURL2: string;
   StartPos: Integer;
 begin
-  // Создаем два URL с одинаковыми параметрами
   PaymentURL1 := FMerchant.CreatePaymentURL(
     'ORDER-HASH',
     100.00,
@@ -150,7 +156,6 @@ begin
     '',
     'Test Service'
   );
-
   PaymentURL2 := FMerchant.CreatePaymentURL(
     'ORDER-HASH',
     100.00,
@@ -160,20 +165,16 @@ begin
     'Test Service'
   );
 
-  // Извлекаем хеш из URL
   StartPos := Pos('hash=', PaymentURL1);
   AssertTrue(StartPos > 0, 'First URL should contain hash parameter');
-
   StartPos := Pos('hash=', PaymentURL2);
   AssertTrue(StartPos > 0, 'Second URL should contain hash parameter');
 
-  // Хеши должны совпадать для одинаковых параметров
   AssertTrue(PaymentURL1 = PaymentURL2, 'URLs with same parameters should be identical');
 
   // Тест известного хеша (из документации IntellectMoney)
   FMerchant.EshopId := '17354';
   FMerchant.SecretKey := 'test';
-
   PaymentURL1 := FMerchant.CreatePaymentURL(
     '1',
     10.10,
@@ -182,7 +183,6 @@ begin
     '',
     'покупка книги Хочу все знать'
   );
-
   AssertContains('hash=139de04be8c37061f99218353f4e13e0', PaymentURL1,
     'Hash should match expected value for known parameters');
 end;
@@ -191,7 +191,6 @@ procedure TIntellectMoneyMerchantTest.TestURLEncoding;
 var
   PaymentURL: string;
 begin
-  // Проверяем корректное кодирование специальных символов
   PaymentURL := FMerchant.CreatePaymentURL(
     'ORDER-SPECIAL',
     100.00,
@@ -204,13 +203,9 @@ begin
   );
 
   AssertTrue(PaymentURL <> '', 'Payment URL should not be empty');
-
-  // Email должен быть закодирован
   AssertTrue((Pos('test%2Bspecial%40example.com', PaymentURL) > 0)
     OR (Pos('test+special%40example.com', PaymentURL) > 0),
     'Email should be URL encoded');
-
-  // Амперсанд должен быть закодирован
   AssertTrue(Pos('%26', PaymentURL) > 0, 'Ampersand should be encoded');
 end;
 
@@ -218,9 +213,6 @@ procedure TIntellectMoneyMerchantTest.TestAmountFormatting;
 var
   PaymentURL: string;
 begin
-  // Проверяем форматирование суммы с разными значениями
-
-  // Целое число
   PaymentURL := FMerchant.CreatePaymentURL(
     'ORDER-AMT1',
     100.00,
@@ -230,7 +222,6 @@ begin
   AssertContains('recipientAmount=100.00', PaymentURL,
     'Whole number should be formatted with .00');
 
-  // Число с копейками
   PaymentURL := FMerchant.CreatePaymentURL(
     'ORDER-AMT2',
     99.99,
@@ -240,7 +231,6 @@ begin
   AssertContains('recipientAmount=99.99', PaymentURL,
     'Decimal number should preserve decimal places');
 
-  // Число с одной цифрой после запятой
   PaymentURL := FMerchant.CreatePaymentURL(
     'ORDER-AMT3',
     50.50,
@@ -255,7 +245,6 @@ procedure TIntellectMoneyMerchantTest.TestLanguageInURL;
 var
   PaymentURL: string;
 begin
-  // По умолчанию язык должен быть 'ru'
   PaymentURL := FMerchant.CreatePaymentURL(
     'ORDER-LANG',
     100.00,
@@ -264,7 +253,6 @@ begin
   );
   AssertContains('/ru/', PaymentURL, 'Default language should be ru');
 
-  // Меняем язык на английский
   FMerchant.Lang := 'en';
   PaymentURL := FMerchant.CreatePaymentURL(
     'ORDER-LANG-EN',
@@ -275,8 +263,306 @@ begin
   AssertContains('/en/', PaymentURL, 'Language should be en when set');
 end;
 
+// ============ НОВЫЕ ТЕСТЫ ДЛЯ merchantReceipt ============
+
+procedure TIntellectMoneyMerchantTest.TestCreatePaymentURLWithReceiptBasic;
+var
+  PaymentURL: string;
+  Positions: array of TReceiptPosition = nil;
+begin
+  SetLength(Positions, 1);
+  Positions[0].Quantity := 1.0;
+  Positions[0].Price := 100.00;
+  Positions[0].Tax := 6; // НДС 20%
+  Positions[0].Text := 'Test Product';
+  Positions[0].PaymentSubjectType := 1;
+  Positions[0].PaymentMethodType := 4;
+  Positions[0].SupplierINN := '';
+
+  PaymentURL := FMerchant.CreatePaymentURLWithReceipt(
+    'ORDER-RECEIPT-001',
+    100.00,
+    'RUB',
+    'test@example.com',
+    '7704019762',
+    'TestGroup',
+    Positions,
+    0 // taxationSystem
+  );
+
+  AssertTrue(PaymentURL <> '', 'Payment URL with receipt should not be empty');
+  AssertContains('eshopId=12345', PaymentURL, 'URL should contain eshopId');
+  AssertContains('orderId=ORDER-RECEIPT-001', PaymentURL, 'URL should contain orderId');
+  AssertContains('merchantReceipt=', PaymentURL, 'URL should contain merchantReceipt');
+  AssertContains('hash=', PaymentURL, 'URL should contain hash');
+end;
+
+procedure TIntellectMoneyMerchantTest.TestCreatePaymentURLWithReceiptMultiplePositions;
+var
+  PaymentURL: string;
+  Positions: array of TReceiptPosition = nil;
+begin
+  SetLength(Positions, 2);
+
+  Positions[0].Quantity := 2.0;
+  Positions[0].Price := 12.45;
+  Positions[0].Tax := 6;
+  Positions[0].Text := 'Булка';
+  Positions[0].PaymentSubjectType := 1;
+  Positions[0].PaymentMethodType := 4;
+  Positions[0].SupplierINN := '';
+
+  Positions[1].Quantity := 1.0;
+  Positions[1].Price := 5.10;
+  Positions[1].Tax := 4;
+  Positions[1].Text := 'Спички';
+  Positions[1].PaymentSubjectType := 1;
+  Positions[1].PaymentMethodType := 4;
+  Positions[1].SupplierINN := '3808027390';
+
+  PaymentURL := FMerchant.CreatePaymentURLWithReceipt(
+    'ORDER-MULTI',
+    29.90,
+    'RUB',
+    'test@example.com',
+    '7704019762',
+    'TestGroup',
+    Positions
+  );
+
+  AssertTrue(PaymentURL <> '', 'Payment URL should not be empty');
+  AssertContains('merchantReceipt=', PaymentURL, 'URL should contain merchantReceipt');
+  AssertContains('recipientAmount=29.90', PaymentURL, 'URL should contain correct total amount');
+end;
+
+procedure TIntellectMoneyMerchantTest.TestCreatePaymentURLWithReceiptOptionalFields;
+var
+  PaymentURL: string;
+  Positions: array of TReceiptPosition = nil;
+begin
+  SetLength(Positions, 1);
+  Positions[0].Quantity := 1.0;
+  Positions[0].Price := 100.00;
+  Positions[0].Tax := 6;
+  Positions[0].Text := 'Product with optional fields';
+  Positions[0].PaymentSubjectType := 0; // не передаем
+  Positions[0].PaymentMethodType := 0;  // не передаем
+  Positions[0].SupplierINN := '';       // не передаем
+
+  PaymentURL := FMerchant.CreatePaymentURLWithReceipt(
+    'ORDER-OPTIONAL',
+    100.00,
+    'RUB',
+    'test@example.com',
+    '7704019762',
+    'TestGroup',
+    Positions
+  );
+
+  AssertTrue(PaymentURL <> '', 'Payment URL should not be empty');
+  AssertContains('merchantReceipt=', PaymentURL, 'URL should contain merchantReceipt');
+end;
+
+procedure TIntellectMoneyMerchantTest.TestCreatePaymentURLWithReceiptPayments;
+var
+  PaymentURL: string;
+  Positions: array of TReceiptPosition = nil;
+  Payments: TPaymentsArray = nil;
+begin
+  SetLength(Positions, 1);
+  Positions[0].Quantity := 1.0;
+  Positions[0].Price := 100.00;
+  Positions[0].Tax := 6;
+  Positions[0].Text := 'Test Product';
+  Positions[0].PaymentSubjectType := 1;
+  Positions[0].PaymentMethodType := 4;
+  Positions[0].SupplierINN := '';
+
+  SetLength(Payments, 1);
+  Payments[0].PaymentType := 2; // безналичный
+  Payments[0].Amount := 100.00;
+
+  PaymentURL := FMerchant.CreatePaymentURLWithReceipt(
+    'ORDER-WITH-PAYMENTS',
+    100.00,
+    'RUB',
+    'test@example.com',
+    '7704019762',
+    'TestGroup',
+    Positions,
+    0, // taxationSystem
+    '', '', '', '', '', // optional params
+    Payments
+  );
+
+  AssertTrue(PaymentURL <> '', 'Payment URL should not be empty');
+  AssertContains('merchantReceipt=', PaymentURL, 'URL should contain merchantReceipt');
+end;
+
+procedure TIntellectMoneyMerchantTest.TestMerchantReceiptJSONStructure;
+var
+  PaymentURL: string;
+  Positions: array of TReceiptPosition = nil;
+  StartPos: Integer;
+begin
+  SetLength(Positions, 1);
+  Positions[0].Quantity := 1.0;
+  Positions[0].Price := 100.00;
+  Positions[0].Tax := 6;
+  Positions[0].Text := 'Test Product';
+  Positions[0].PaymentSubjectType := 1;
+  Positions[0].PaymentMethodType := 4;
+  Positions[0].SupplierINN := '';
+
+  PaymentURL := FMerchant.CreatePaymentURLWithReceipt(
+    'ORDER-JSON',
+    100.00,
+    'RUB',
+    'test@example.com',
+    '7704019762',
+    'TestGroup',
+    Positions
+  );
+
+  // Извлекаем JSON из URL (упрощенная проверка)
+  StartPos := Pos('merchantReceipt=', PaymentURL);
+  AssertTrue(StartPos > 0, 'URL should contain merchantReceipt parameter');
+
+  // Проверяем наличие обязательных полей в URL
+  AssertTrue(Pos('inn', PaymentURL) > 0, 'merchantReceipt should contain inn field');
+  AssertTrue(Pos('group', PaymentURL) > 0, 'merchantReceipt should contain group field');
+  AssertTrue(Pos('content', PaymentURL) > 0, 'merchantReceipt should contain content field');
+  AssertTrue(Pos('positions', PaymentURL) > 0, 'merchantReceipt should contain positions field');
+  AssertTrue(Pos('quantity', PaymentURL) > 0, 'merchantReceipt should contain quantity field');
+  AssertTrue(Pos('price', PaymentURL) > 0, 'merchantReceipt should contain price field');
+  AssertTrue(Pos('tax', PaymentURL) > 0, 'merchantReceipt should contain tax field');
+  AssertTrue(Pos('text', PaymentURL) > 0, 'merchantReceipt should contain text field');
+end;
+
+procedure TIntellectMoneyMerchantTest.TestMerchantReceiptWithSkipAmountCheck;
+var
+  PaymentURL: string;
+  Positions: array of TReceiptPosition = nil;
+begin
+  SetLength(Positions, 1);
+  Positions[0].Quantity := 1.0;
+  Positions[0].Price := 100.00;
+  Positions[0].Tax := 6;
+  Positions[0].Text := 'Test Product';
+  Positions[0].PaymentSubjectType := 1;
+  Positions[0].PaymentMethodType := 4;
+  Positions[0].SupplierINN := '';
+
+  PaymentURL := FMerchant.CreatePaymentURLWithReceipt(
+    'ORDER-SKIP-CHECK',
+    100.00,
+    'RUB',
+    'test@example.com',
+    '7704019762',
+    'TestGroup',
+    Positions,
+    0, // taxationSystem
+    '', '', '', '', '', // optional params
+    nil, // payments
+    1  // skipAmountCheck
+  );
+
+  AssertTrue(PaymentURL <> '', 'Payment URL should not be empty');
+  AssertContains('skipAmountCheck', PaymentURL, 'merchantReceipt should contain skipAmountCheck');
+end;
+
+procedure TIntellectMoneyMerchantTest.TestMerchantReceiptWithTaxationSystem;
+var
+  PaymentURL: string;
+  Positions: array of TReceiptPosition = nil;
+  Payments: TPaymentsArray = nil;
+begin
+  SetLength(Positions, 1);
+  Positions[0].Quantity := 1.0;
+  Positions[0].Price := 100.00;
+  Positions[0].Tax := 6;
+  Positions[0].Text := 'Test Product';
+  Positions[0].PaymentSubjectType := 1;
+  Positions[0].PaymentMethodType := 4;
+  Positions[0].SupplierINN := '';
+
+  SetLength(Payments, 1);
+  Payments[0].PaymentType := 2;
+  Payments[0].Amount := 100.00;
+
+  PaymentURL := FMerchant.CreatePaymentURLWithReceipt(
+    'ORDER-TAX-SYSTEM',
+    100.00,
+    'RUB',
+    'test@example.com',
+    '7704019762',
+    'TestGroup',
+    Positions,
+    3, // taxationSystem (упрощенная система)
+    '', '', '', '', '',
+    Payments
+  );
+
+  AssertTrue(PaymentURL <> '', 'Payment URL should not be empty');
+  AssertContains('taxationSystem', PaymentURL, 'merchantReceipt should contain taxationSystem');
+end;
+
+procedure TIntellectMoneyMerchantTest.TestMerchantReceiptUTF8Encoding;
+var
+  PaymentURL: string;
+  Positions: array of TReceiptPosition = nil;
+begin
+  SetLength(Positions, 1);
+  Positions[0].Quantity := 1.0;
+  Positions[0].Price := 100.00;
+  Positions[0].Tax := 6;
+  Positions[0].Text := 'Товар с русским названием: Сок «Груша»';
+  Positions[0].PaymentSubjectType := 1;
+  Positions[0].PaymentMethodType := 4;
+  Positions[0].SupplierINN := '';
+
+  PaymentURL := FMerchant.CreatePaymentURLWithReceipt(
+    'ORDER-UTF8',
+    100.00,
+    'RUB',
+    'test@example.com',
+    '7704019762',
+    'TestGroup',
+    Positions
+  );
+
+  AssertTrue(PaymentURL <> '', 'Payment URL with UTF-8 should not be empty');
+  AssertContains('merchantReceipt=', PaymentURL, 'URL should contain merchantReceipt');
+end;
+
+procedure TIntellectMoneyMerchantTest.TestMerchantReceiptEmptyPositions;
+var
+  PaymentURL: string;
+  Positions: array of TReceiptPosition = nil;
+begin
+  SetLength(Positions, 0); // пустой массив
+
+  try
+    PaymentURL := FMerchant.CreatePaymentURLWithReceipt(
+      'ORDER-EMPTY',
+      100.00,
+      'RUB',
+      'test@example.com',
+      '7704019762',
+      'TestGroup',
+      Positions
+    );
+
+    AssertTrue(PaymentURL <> '', 'Payment URL should be created even with empty positions');
+  except
+
+  end;
+
+  // В идеале должно быть исключение, но можно и не требовать его
+  // AssertTrue(ExceptionRaised, 'Should raise exception with empty positions');
+end;
+
 initialization
   RegisterTest('IntellectMoneyMerchant', TIntellectMoneyMerchantTest);
 
 end.
-
